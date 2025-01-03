ce0a01eaec87c72a31ee2d62b83cec3815eaebc6
AnantLad <anantlad018@gmail.com>
2025-01-03 22:36:20 +0530

    Create Product.java

---
+++ CollectionFramework/Product.java
@@ -0,0 +1,43 @@
+package CollectionFramework;
+
+import java.util.Arrays;
+
+public class Product {
+    static int[] productExceptSelf(int[] nu,int n){
+        int c = 0;
+        int prod = 1;
+        int[] res = new int[n]; 
+        Arrays.fill(res,0);
+        for( int num : nu){
+            if (num == 0) 
+                 c++;
+            else
+                prod *= num;  
+        }
+        for(int i = 0; i < n; i++){
+            if(c>1)
+            res[i] = 0;
+            else if(c == 1){
+                if (nu[i]==0) 
+                res[i] = prod;
+                else
+                res[i] = 0;   
+            }
+            else{
+                res[i] = prod/nu[i];
+            }
+        }
+        return res;
+    } 
+
+    public static void main(String[] args) {
+        int [] nu = {2,5,3,4,1};
+        int [] result = productExceptSelf(nu,nu.length);
+    
+        for(int val : result){
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+}
+  
