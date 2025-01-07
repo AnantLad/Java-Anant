dbc9a8b9158ab9b65220cf19238e0b5c74a695af
AnantLad <anantlad018@gmail.com>
2025-01-07 22:33:58 +0530

    Create Rotated.java

---
+++ CollectionFramework/Rotated.java
@@ -0,0 +1,27 @@
+package CollectionFramework;
+
+public class Rotated {
+    static int findMin(int[] arr){
+        int low =0 , high = arr.length -1;
+
+        while (low<high) {
+            if (arr[low] < arr[high]) 
+                return low;
+
+            int mid = (low + high)/2;
+            
+            if(arr[mid] > arr[high])
+                low = mid + 1;
+
+            else
+                high = mid;
+        }
+        return arr[low];
+
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {5,6,7,8,1,2,3,4};
+        System.out.println(findMin(arr));
+    }
+}
