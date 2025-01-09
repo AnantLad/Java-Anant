981bbc63492e849bbcce2b73625feddd3b6d992a
AnantLad <anantlad018@gmail.com>
2025-01-09 22:35:00 +0530

    Create MostWater.java

---
+++ CollectionFramework/MostWater.java
@@ -0,0 +1,26 @@
+package CollectionFramework;
+
+public class MostWater {
+
+    static int maxWater(int [] arr){
+        int left = 0, right = arr.length - 1;
+        int res = 0;
+        while(left<right){
+
+            int water = Math.min(arr[left] , arr[right])*(right - left);
+
+            res = Math.max(res, water);
+
+            if( arr[left] < arr[right])
+            left += 1;
+
+           else
+            right -= 1; 
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int [] arr = { 2,1,3,6,8,5,4,8};
+        System.out.println(maxWater(arr));
+    }
+}
